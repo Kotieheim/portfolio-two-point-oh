2bcf2d7f4d1c9bbf747c369bdaf3a63ca2d0fd4e
Dakota Heim <kotieheim@gmail.com>
2022-03-09 19:29:20 -0500

    header changes and styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,39 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Header.module.css";
+import logoImage from "../public/logoipsum-logo-37.svg";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>DH</div>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_main}>
+        <Link href="/contact">
+          <a className={styles.navbar_main__link}>Contact</a>
+        </Link>
+        <Link href="/work">
+          <a className={styles.navbar_main__link}>Work</a>
+        </Link>
+      </div>
+      <div className={styles.navbar_logo}>
+        <Link href="/">
+          <Image
+            width={30}
+            alt="logo"
+            src={logoImage}
+            className={styles.navbar_logo__link}
+          />
+        </Link>
+      </div>
+      <div className={styles.navbar_socials}>
+        <Link href="/">social 1</Link>
+        <Link href="/">social 1</Link>
+      </div>
+
       {/* <Link href="/Other/Test">Test?</Link> */}
-    </div>
+    </nav>
   );
 };
 
